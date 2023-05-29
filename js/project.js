aa22f0fc86ea2e23775a25ffa38d397a7c8e73dc
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2023-05-29 20:54:34 -0300

    seta-back

--- js/project.js
+++ js/project.js
@@ -1,5 +1,10 @@
 const imgTrack = document.getElementById("image-track");
 const imgs = document.querySelectorAll(".image");
+const arrow = document.querySelector(".arrow");
+//
+const imgSlide = document.querySelector(".image-slide");
+const imgsR = document.querySelector(".images");
+//
 
 function addClass() {
   imgTrack.removeAttribute("id");
@@ -8,7 +13,23 @@ function addClass() {
     i.classList.remove("image");
     i.classList.add("images");
   });
+  arrow.style.display = "block";
 }
+//
+function removeClass() {
+  if (imgSlide) {
+    imgSlide.classList.remove(imgSlide);
+    imgSlide.classList.add(imgTrack);
+    imgsR.forEach((i) => {
+      i.classList.remove(imgsR);
+      i.classList.add(imgs);
+    });
+  }
+}
+//
+
 imgs.forEach((i) => {
   i.onclick = addClass;
 });
+
+arrow.onclick = removeClass;
