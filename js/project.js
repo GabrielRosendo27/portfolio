9cbe5fa287e8419a4e8a1d02360c234b3d15a8d5
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2023-05-30 16:45:13 -0300

    opacity

--- js/project.js
+++ js/project.js
@@ -20,12 +20,37 @@ function removeClass() {
       i.classList.remove("images");
       i.classList.add("image");
       arrow.style.display = "none";
+      i.style.opacity = "1";
     });
   }
 }
 //
+function showProject() {
+  const actions = {
+    0: () => {
+      console.log("Clicou na imagem da posição 0");
+    },
+    1: () => {
+      console.log("Clicou na imagem da posição 1");
+    },
+  };
+  imgs.forEach((i, p) => {
+    i.addEventListener("click", () => {
+      imgs.forEach((i) => {
+        i.style.opacity = "1";
+      });
+      if (actions[p]) {
+        actions[p]();
+        i.style.opacity = "0.3";
+      } else {
+        console.log("Clicou em outra imagem");
+      }
+    });
+  });
+}
 imgs.forEach((i) => {
   i.onclick = addClass;
+  showProject();
 });
 //
 arrow.onclick = removeClass;
