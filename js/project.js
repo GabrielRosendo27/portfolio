10b7210f59c6d5ffd1b3caa4347a50e746a6f4ed
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-08-13 12:11:39 -0300

    add-project

--- js/project.js
+++ js/project.js
@@ -60,6 +60,9 @@ document.addEventListener("DOMContentLoaded", function () {
       4: () => {
         projectFive();
       },
+      5: () => {
+        projectSix();
+      },
     };
     //
     imgs.forEach((i, p) => {
@@ -89,12 +92,12 @@ document.addEventListener("DOMContentLoaded", function () {
   //
   arrow.onclick = removeClass;
   //
-  function projectOne() {
+  function projectSix() {
     resetProject();
     imgProject.insertAdjacentHTML("afterbegin", '<img src="./img/bikcraft.png" draggable="false" class="x" />');
     DescH1.innerHTML = "Bikcraft";
     DescP.innerHTML =
-      "<br><br>Site fictício de uma empresa chamada Bikcraft feito apenas com HTML e CSS. <br><br>• Contém 6 páginas, Home, Orçamento, Seguros, Termos, Bicicletas e Contato.<br><br> Foi fundamental para consolidar meus conhecimentos nessas tecnologias e em como elas interagem entre si.";
+      "<br>Site fictício de uma empresa chamada Bikcraft feito apenas com HTML e CSS. <br><br>Contém 6 páginas<br> • Home <br>• Orçamento <br>• Seguros<br>• Termos<br>• Bicicletas <br>• Contato.<br><br> Foi fundamental para consolidar meus conhecimentos nessas tecnologias e em como elas interagem entre si.";
     DescLink.setAttribute("href", "https://github.com/GabrielRosendo27/projetobikcraft");
     const valores = {
       0: '<img src="img/html.svg" draggable="false" class="icon-svg" />',
@@ -106,7 +109,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //
   }
   //
-  function projectTwo() {
+  function projectFive() {
     resetProject();
     imgProject.insertAdjacentHTML("afterbegin", '<img src="./img/jogodavelha.png" draggable="false" class="x-s" />');
     DescH1.innerHTML = "Jogo Da Velha";
@@ -160,16 +163,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   //
-  function projectFive() {
+  function projectTwo() {
     resetProject();
     imgProject.insertAdjacentHTML("afterbegin", '<img src="./img/botDiscImg/imgBotDisc.PNG" draggable="false" class="x-s" />');
     DescH1.innerHTML = "Discord Bot";
     DescP.innerHTML =
-      "<br>Bot de Discord que utiliza Web Scrapping, feito em python, para conseguir informações de jogadores de um jogo chamado Tibia.<br><br> O bot, a partir de um comando específico recebido pelo Discord, executa operações assíncronas de Scrapping no site escolhido e recolhe as informações pertinentes e as traz de volta em forma de mensagem no próprio Discord.<br><br> Exemplo utilizando o comando '!add *NomeDoPersonagem*'<br>";
+      "<br>Bot de Discord que utiliza Web Scraping, feito em Python, para obter informações de jogadores de um jogo chamado Tibia.<br><br> O bot, a partir de um comando específico recebido pelo Discord, executa operações assíncronas de scraping no site escolhido, recolhe as informações pertinentes e as traz de volta em forma de mensagem no próprio Discord.<br><br> Exemplo utilizando o comando '!add *NomeDoPersonagem*'<br>";
     DescP.insertAdjacentHTML("beforeend", '<br><img src="./img/botDiscImg/comandoAdd.PNG" alt="Imagem do Bot de Discord" class="x-x" /> <br> ');
     DescP.insertAdjacentHTML(
       "beforeend",
-      "<br>Após utilizar o comando, o bot retorna as informações do personagem diretamente no canal do Discord.<br> Após adicionado um personagem através do nome, ele fica com suas informações salvas no banco de dados SQLite.<br><br> Uma vez salvo o personagem, o bot também faz uma verificação, a cada minuto após ser iniciado, onde ele compara as informações do site com as encontradas no banco de dados, e se há alguma alteração ele informa qual mudança de estado teve naquele personagem. Ele faz isso com todos os personagens no banco de dados. <br> Exemplo de alterações no estado de um personagem: <br>"
+      "<br>Após utilizar o comando, o bot retorna as informações do personagem diretamente no canal do Discord.<br>Adicionado um personagem através do nome, ele fica com suas informações salvas no banco de dados SQLite.<br><br> Uma vez salvo o personagem, o bot também faz uma verificação, a cada minuto após ser iniciado, onde ele compara as informações do site com as encontradas no banco de dados, e se há alguma alteração ele informa qual mudança de estado teve naquele personagem. Ele faz isso com todos os personagens no banco de dados. <br> Exemplo de alterações no estado de um personagem: <br>"
     );
     DescP.insertAdjacentHTML("beforeend", '<br><img src="./img/botDiscImg/botStatus.PNG" alt="Imagem do Bot de Discord" class="x-x" /> <br> ');
     DescP.insertAdjacentHTML(
@@ -191,6 +194,22 @@ document.addEventListener("DOMContentLoaded", function () {
       item.innerHTML = valores[pos];
     });
     //
+  }
+  function projectOne() {
+    resetProject();
+    imgProject.insertAdjacentHTML("afterbegin", '<img src="./img/applistafilmesx.PNG" draggable="false" class="x" />');
+    DescH1.innerHTML = "Lista Filmes App (Não Finalizado)";
+    DescP.innerHTML =
+      "<br>Projeto pessoal de uma lista de filmes feito em React.<br> Possuo uma lista de filmes em um bloco de notas, e resolvi criar uma aplicação em cima disso, aproveitando para aprender mais sobre React.<br><br> A aplicação consiste em fazer uma busca de um filme com o nome do título podendo ser em português ou inglês.<br><br>Na aplicação fazemos requisições a duas APIs gratuitas diferentes, a API do OMDB (Open Movie Database) e a API do TMDB (The Movie Database).<br><br> As duas APIs são utilizadas como complemento a cada uma, fornecendo informações diferentes sobre cada filme, a API do OMDB não suporta pesquisa de títulos em diferentes idiomas, então é necessário o uso de uma segunda API como a do TMDB pois na minha lista os filmes estão com o título em português.<br><br>Então primero fazemos a pesquisa de um filme que faz a requisição na API do TMDB, dessa API o que retornamos é: o título do filme (em português), o título original do filme (o qual é usado para pesquisar na API do OMDB para o resto das informações), a sinopse ou descrição do filme, e o caminho para imagem do pôster do filme.<br><br> Ja a API do OMDB é usada para complementar as informações recebidas da API do TMDB, ela retorna dados como: ano de lançamento, duração, gênero, país de origem, avaliação no IMDB, avaliação e classificação no Rotten Tomatoes.<br><br> Os dados de ambas APIs são combinados e apresentados fornecendo informações mais detalhadas de cada filme.<br><br> Os filmes adicionados e seus respectivos dados são salvos no banco de dados do Firebase.<br><br>Também é possível remover o filme da lista clicando no ícone de lixeira em cada filme e que também é apagado do banco de dados. <br><br>";
+    DescP.innerHTML += '<a href="https://lista-filmes-app.vercel.app/" target="_blank">Projeto ↵</a>';
+    DescLink.setAttribute("href", "https://github.com/GabrielRosendo27/ListaFilmesApp");
+    const valores = {
+      0: '<img src="img/react.svg" draggable="false" class="icon-svg" />',
+      1: '<img src="img/firebase.svg" draggable="false" class="icon-svg" />',
+    };
+    TecnoLi.forEach((item, pos) => {
+      item.innerHTML = valores[pos];
+    });
   }
   function resetProject() {
     const imgX = document.querySelector(".img-project img");
